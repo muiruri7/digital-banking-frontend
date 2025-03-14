b39eb5d819118b56709bac7e90b94b2d05d2b496
muiruri7 <leon.w.muiruri@gmail.com>
2025-03-14 16:11:50 +0300

    Phase 1: Initial project setup with Angular CLI, Bootstrap, and module structure

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://172.16.2.171',
+  // You can later add authentication settings here (e.g., endpoints for login, token refresh, etc.)
+};
